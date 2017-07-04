0935bb4ef332d3b91297dd36dc8fa183cba62afe
Matias Vargas <matias.vargasm@usach.cl>
2017-07-04 16:33:27 +0000

    Inicializa PythonService

--- app/Services/PythonService.php
+++ app/Services/PythonService.php
@@ -18,10 +18,11 @@ class PythonService
     public function callDato(Request $request)
     {
         $var1 = Carbon::now()->toDateTimeString();
-        $var2 = -30.321;
-        $var3 = -40.432;
-        $var4 = -45.421;
-        $var5 = -34.213;
+        $var2 = (float)((request()->lat1)?request()->lat1:-30.321);
+        $var3 = (float)((request()->lon1)?request()->lon1:-40.432);
+        $var4 = (float)((request()->lat2)?request()->lat2:-45.421);
+        $var5 = (float)((request()->lon2)?request()->lon2:-34.213);
+        dd([$var2, $var3, $var4, $var5]);
         $output = null;
         $dato = system('python3.5 public/python/predecirTiempoViajeOk.py '.$var1.' '.$var2.' '.$var3.' '.$var4.' '.$var5, $output);
         return $output;
